b8b461341fed2bdc265fb31ef3147064bc321148
Sultész Lehel <67593@4ig.hu>
2022-02-16 21:10:03 +0100

    Added Listing Paired and Non-paired devices ,shows null devices too , Added Bluetooth On button and Menus The app can now search for BT devices

--- app/src/main/java/com/example/bluetoothbikeapp/BluetoothDeviceActivity.java
+++ app/src/main/java/com/example/bluetoothbikeapp/BluetoothDeviceActivity.java
@@ -1,9 +1,22 @@
 package com.example.bluetoothbikeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,11 +25,20 @@ public class BluetoothDeviceActivity extends AppCompatActivity {
     private static final String LOG_TAG = BluetoothDeviceActivity.class.getName();
     private FirebaseUser user;
     private FirebaseAuth mAuth;
+    private Context context;
+    private BluetoothAdapter bluetoothAdapter;
+
+    private final int LOCATION_PERMISSION_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bluetoothdevice_activity);
+
+
+        context = this;
+        initBluetooth();
+
         mAuth = FirebaseAuth.getInstance();
         // mAuth.signOut();
         user = FirebaseAuth.getInstance().getCurrentUser();
@@ -26,6 +48,89 @@ public class BluetoothDeviceActivity extends AppCompatActivity {
         } else {
             Log.d(LOG_TAG, "Unauthenticated user!");
             finish();
+        }
+    }
+
+    private void initBluetooth() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(context, "No bluetooth found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bluetooth_device_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_search_devices:
+                //Toast.makeText(context,"Clicked Search Devices!",Toast.LENGTH_SHORT).show();
+               checkPermissions();
+                return true;
+            case R.id.menu_enable_bluetooth:
+
+                enableBluetooth();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(BluetoothDeviceActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
+        } else {
+            Intent intent = new Intent(context, DeviceListActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(context, DeviceListActivity.class);
+                startActivity(intent);
+            } else {
+                new AlertDialog.Builder(context)
+                        .setCancelable(false)
+                        .setMessage("Location permission is required.\n Please grant")
+                        .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                checkPermissions();
+                            }
+                        })
+                        .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                BluetoothDeviceActivity.this.finish();
+                            }
+                        }).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    private void enableBluetooth() {
+        if (bluetoothAdapter.isEnabled()) {
+            Toast.makeText(context, "Bluetooth Already Enabled", Toast.LENGTH_SHORT).show();
+        }else{
+            bluetoothAdapter.enable();
+        }
+
+        if (bluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoveryIntent);
         }
     }
 }
