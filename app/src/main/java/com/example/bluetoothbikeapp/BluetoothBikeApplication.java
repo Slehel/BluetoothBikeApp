7d6131c67ef3a738bdc0c555217dc89ccffffa2f
Ferenc Soós <pkferi@gmail.com>
2022-03-11 20:49:42 +0100

    Fix darkmode

---
+++ app/src/main/java/com/example/bluetoothbikeapp/BluetoothBikeApplication.java
@@ -0,0 +1,14 @@
+package com.example.bluetoothbikeapp;
+
+import android.app.Application;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class BluetoothBikeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+}
